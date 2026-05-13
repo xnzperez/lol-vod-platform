518209f9ee414baef67d61d4f4018c1f6ee7728c
Carlos Pérez <carlospzk31@gmail.com>
2026-05-13 16:32:23 -0500

    fix: eliminación manualChunks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,29 +3,15 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            // Separa el core de React
-            if (
-              id.includes("react") ||
-              id.includes("react-dom") ||
-              id.includes("react-router-dom")
-            ) {
-              return "vendor-react";
-            }
-            // Separa UI y estilos
-            if (id.includes("sileo") || id.includes("lucide-react")) {
-              return "vendor-ui";
-            }
-            // Resto de dependencias
-            return "vendor-core";
-          }
-        },
-      },
-    },
-  },
+        manualChunks: undefined 
+      }
+    }
+  }
 });
